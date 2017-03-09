6d43c2bfc3ae4663c70bafb855f1415b3cb85aad
Shawon Ashraf <shawon13@live.com>
2017-03-10 00:02:46 +0600

    Added files

---
+++ README.md
@@ -0,0 +1,35 @@
+# Getting your course wise schedule from the PDF published on NSU Website
+###### Just run it!
+
+### How it works?
+
+- [x] Uses PyPDF2 module to read all the text in the PDF file
+- [x] Then a brute force algorithm that searches the entire file, all the text, one by one.
+- [x] Search params : courseID and section number. Apparently that's what you use to look for right?
+- [x] If your course is found, will return courseID, section, date, time and room number of exam
+- [x] Since a brute force algorithm runs in, it's slow. It was a quick hack, so no optimization. (You can if you want,
+    contributions are welcome!)
+
+### Usage
+
+- [x] Clone the repo or download as a zip
+- [x] Get into the folder
+    `cd "Final Exam Schedule Getter" `
+- [x] Then run the following command in your command line to make sure you have the necessary modules ready(skip if you
+    already know how to get modules from a requirements file)
+
+    `pip install -r requirements.txt`
+     (If you're using anaconda then instead of pip,
+     use [this shell script to do things](https://gist.github.com/ShawonAshraf/1ee95026b80838d3f51776a5bdfcd2d4))
+
+- [x] Then navigate to sources folder
+    `cd sources`
+
+- [x] Run the Python script :
+    `python schedule_getter.py [params]`
+    params are : your courseId and section, separated by a space
+
+    __Example :__
+    `python schedule_getter.py CSE327 3 CSE418 1 CSE323 2 PHY108 3`
+
+    **Make sure courseID has no space in it, e.g. typing CSE 327 instead of CSE327 is an error here**
---
+++ sources/schedule_getter.py
@@ -0,0 +1,60 @@
+import PyPDF2
+import sys
+
+
+class Schedule:
+    def __init__(self, courseID, section, date, time, room):
+        self.courseID = courseID
+        self.section = section
+        self.date = date
+        self.time = time
+        self.room = room
+
+    def __str__(self):
+        return (self.courseID + "\t"
+                + self.section + "\t"
+                + self.date + "\t"
+                + self.time + "\t"
+                + self.room + "\n"
+                )
+
+
+class ScheduleExtract:
+    numPages = 36
+
+    def readPage(self, pageNumber):
+        pdfFile = open('../pdf/Exam_Schedule_171_revised.pdf', 'rb')
+        reader = PyPDF2.PdfFileReader(pdfFile)
+
+        pageObj = reader.getPage(pageNumber)
+        dataObj = pageObj.extractText()
+
+        pdfFile.close()
+
+        data = dataObj.split('\n')
+
+        return data
+
+    def getSchedule(self, courseID, section):
+        for pageNo in range(ScheduleExtract.numPages):
+            dataFromPage = self.readPage(pageNo)
+
+            for i in range(len(dataFromPage)):
+                if dataFromPage[i] == courseID and dataFromPage[i + 1] == section:
+                    schedule = Schedule(dataFromPage[i], dataFromPage[i + 1], dataFromPage[i + 2],
+                                        dataFromPage[i + 3], dataFromPage[i + 4])
+
+                    return schedule
+                else:
+                    continue
+
+
+# main tasks here
+
+extractor = ScheduleExtract()
+
+courseFromCmd = sys.argv
+
+for i in range(1, len(sys.argv), 2):
+    sc = extractor.getSchedule(courseFromCmd[i], courseFromCmd[i + 1])
+    print(sc)
